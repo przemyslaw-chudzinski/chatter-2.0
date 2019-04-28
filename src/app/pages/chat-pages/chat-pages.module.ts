b789bc2e380600edcf75b6a837c0d88ba93e52b3
Przemysław Chudziński <przemyslaw-chudzinski@wp.pl>
2019-04-28 15:32:40 +0200

    working on project

--- src/app/pages/chat-pages/chat-pages.module.ts
+++ src/app/pages/chat-pages/chat-pages.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { ChatPagesComponent } from './chat-pages.component';
 import { IndexPageComponent } from './index-page/index-page.component';
 import {RouterModule, Routes} from '@angular/router';
+import {ContactsModule} from '../../contacts/contacts.module';
+import {UiModule} from '../../ui/ui.module';
 
 const routes: Routes = [
   {
@@ -15,7 +17,9 @@ const routes: Routes = [
   declarations: [ChatPagesComponent, IndexPageComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    ContactsModule,
+    UiModule
   ]
 })
 export class ChatPagesModule { }
